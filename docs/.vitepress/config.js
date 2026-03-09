e0cfb8d43114b7cfc399fdef8956890aebde5055
Zrokei <2842809454@qq.com>
2026-03-09 22:22:48 +0800

    fix:修改config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,5 +7,6 @@ export default {
   head: [
     // Meta for mobile
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }]
+    ['link', { rel: 'icon', href: '/logo.png' }]
   ]
 }
