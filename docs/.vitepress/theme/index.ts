34471279762093860a32ddb597475ef80dce4717
Zrokei <2842809454@qq.com>
2026-03-11 17:01:31 +0800

    fix:增加了rainbow.css文件,在index.ts中补充代码

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,39 @@
 // 引入 VitePress 默认主题
 import DefaultTheme from 'vitepress/theme'
 import './style/index.css'
+// 彩虹背景动画样式
+let homePageStyle: HTMLStyleElement | undefined
 
 export default {
   extends: DefaultTheme, // 继承默认主题
   enhanceApp({ app, router, siteData }) {
     // 可以在这里注册全局组件
+    // 彩虹背景动画样式
+    if (typeof window !== 'undefined') {
+      watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === '/'),
+        { immediate: true },
+      )
+    }
+  }
+}
+
+// 彩虹背景动画样式
+function updateHomePageStyle(value: boolean) {
+  if (value) {
+    if (homePageStyle) return
+
+    homePageStyle = document.createElement('style')
+    homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`
+    document.body.appendChild(homePageStyle)
+  } else {
+    if (!homePageStyle) return
+
+    homePageStyle.remove()
+    homePageStyle = undefined
   }
 }
