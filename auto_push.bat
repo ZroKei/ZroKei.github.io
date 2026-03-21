78769c57e0595f753b792c638798f386d547b751
Zrokei <2842809454@qq.com>
2026-03-21 22:33:23 +0800

    fix:对.bat文件修改，修改自动检测提交时的备注名称

--- auto_push.bat
+++ auto_push.bat
@@ -1,4 +1,4 @@
 git add .
 :: 如果没有更改则不提交，避免报错
-git diff-index --quiet HEAD || git commit -m "Auto update blog content"
+git diff-index --quiet HEAD || git commit -m "Auto update blog:对.md文档进行更新"
 git push
